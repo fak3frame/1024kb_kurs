9115615a3effb84274b838ae18f02081a9f4d386
kamil7745@gmail.com <01074421gbGB>
2019-07-08 15:12:51 +0200

    info 1

--- src/One.java
+++ src/One.java
@@ -1,6 +1,10 @@
-public class Main {
+public class One {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        final int zmiennaStala;
+        zmiennaStala = 20;
+
     }
 }
