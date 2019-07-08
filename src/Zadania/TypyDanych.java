9b1fd53b52bf2fea6009a9054a5b9bb48f2b9478
kamil7745@gmail.com <01074421gbGB>
2019-07-08 23:02:44 +0200

    zmienne proste

---
+++ src/Zadania/TypyDanych.java
@@ -0,0 +1,16 @@
+package Zadania;
+
+public class TypyDanych {
+    public static void main(String[] args) {
+        int a = 35;
+        float b = 43.4f;
+        char c = 'a';
+        boolean d = true;
+        char e = 'b';
+        boolean f = false;
+        float g = 95.4f;
+        long h = 30120320323l;
+        double i = 2.8123123123;
+        double j = 33.;
+    }
+}
