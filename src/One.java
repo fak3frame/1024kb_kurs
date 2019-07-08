a55d98378d26767eb860e23a6105306049e417ef
kamil7745@gmail.com <01074421gbGB>
2019-07-08 15:57:07 +0200

    info 2

--- src/One.java
+++ src/One.java
@@ -5,6 +5,22 @@ public class One {
 
         final int zmiennaStala;
         zmiennaStala = 20;
+        //zmiennaStala = 30; - blad - nadana juz jeden raz wartosc!
+
+        long zmiennaLong = 30;
+        long zmiennaLong2 = 23L;
+        long zmiennaLong3 = 23l;
+
+        float zmiennaFloat1 = 2.3f;
+        float zmiennaFloat2 = 2.3F;
+        //float zmiennaFloat3 = 2.3; - blad musi byc f/F
+
+        double zmiennaDouble1 = 2.3;
+        double zmiennaDouble2 = 2.3d;
+        double zmiennaDouble3 = 2.3D;
+
+
+
 
     }
 }
